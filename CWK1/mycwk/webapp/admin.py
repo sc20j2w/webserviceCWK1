bffba20fbdd0d5773771ce9c95324ad6aff8bec5
AveryWang <101088483+sc20j2w@users.noreply.github.com>
2024-04-19 20:37:26 +0800

    Delete CWK1 directory

--- CWK1/mycwk/webapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Story, Author
-admin.site.register(Story)
-admin.site.register(Author)
--- CWK1/mycwk/webapp/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import login, logout, delete, StoryView
-from django.urls import path
-
-urlpatterns = [
-    path('api/stories', StoryView.as_view(), name='stories'),
-    path('api/stories/<int:key>', delete, name='delete_story'),
-    path('api/login', login, name='login'),
-    path('api/logout', logout, name='logout'),
-]
